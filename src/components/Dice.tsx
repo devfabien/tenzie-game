3980ae990942fb8346a9c86046c51324b43ee659
devfabien <ish001fabi@gmail.com>
2023-10-10 17:56:03 +0200

    using the prop interface

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -1,7 +1,15 @@
-export default function Dice({value}:{value:number|string}){
-    return(
-        <div className="border hover:bg-slate-100 my-3 px-4 py-2 rounded-md flex items-center shadow-md justify-center">
-            <button className="text-4xl font-bold ">{value}</button>
-        </div>
-    )
+import { Props } from "./interfaces";
+
+export default function Dice({
+  value,
+  isHeld,
+  holdDice,
+}: Props) {
+  return (
+    <div onClick={holdDice}
+      className={` border my-3 px-4 py-2 rounded-md flex items-center shadow-md justify-center`}
+    >
+      <button className="text-4xl font-bold ">{value}</button>
+    </div>
+  );
 }
