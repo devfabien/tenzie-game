d09e66246c6c33b4dec74ad179390c3fc77b4cfe
devfabien <ish001fabi@gmail.com>
2023-10-10 17:57:01 +0200

    adding the styles for the isHeld property

--- src/components/Dice.tsx
+++ src/components/Dice.tsx
@@ -7,7 +7,9 @@ export default function Dice({
 }: Props) {
   return (
     <div onClick={holdDice}
-      className={` border my-3 px-4 py-2 rounded-md flex items-center shadow-md justify-center`}
+      className={` border my-3 px-4 py-2 rounded-md flex items-center shadow-md justify-center ${
+        isHeld? "bg-green-500" :"bg-white"
+      }`}
     >
       <button className="text-4xl font-bold ">{value}</button>
     </div>
